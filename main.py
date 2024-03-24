bc546b674d41efc529fb17889dc4b8edad98ddc2
Hasif Ahmed <142938794+hasifnumerics@users.noreply.github.com>
2024-03-24 16:51:21 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+import sys
+
+# set up pygame
+pygame.init()
+
+# set up the window
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Welcome to Making a Game of It')
+
+# set up the colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# set up fonts
+basicFont = pygame.font.SysFont(None, 48)
+
+# set up the text
+text = basicFont.render('Hello World!', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
+
+# draw the text onto the surface
+windowSurface.blit(text, textRect)
+
+# draw the window onto the screen
+pygame.display.update()
+
+# run the game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
